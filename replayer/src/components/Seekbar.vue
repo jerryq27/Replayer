162dfe3f0364dd1bc8bc9db22d61fce78b0b162d
Jerry Quintero <jerryq27@gmail.com>
2020-08-20 20:17:22 -0500

    Created a seekbar, using Bootstrap instead.

--- replayer/src/components/Seekbar.vue
+++ replayer/src/components/Seekbar.vue
@@ -1,11 +1,19 @@
 <template>
   
   <div>
-      <p id="time">{{ updateCurrentTime() }} - {{ formatDuration }}</p>
-      <div id="seekbar">
-          <div id="length"></div>
-          <div id="fill" v-bind:style="styles.fill"></div>
-          <div id="thumb"></div>
+      <p class="time">{{ updateCurrentTime() }} - {{ formatDuration }}</p>
+      <div class="seekbar">
+          <div
+            v-bind:style="fill"
+            class="seekbar-fill">
+          </div>
+          <div
+            class="seekbar-thumb"
+            v-bind:style="thumb"
+            v-on:mousedown="startScrub"
+            v-on:mouseup="endScrub">
+          </div>
+          <div class="seekbar-length"></div>
       </div>
   </div>
 
@@ -22,13 +30,11 @@ export default {
         return {
             durationString: '',
             currentTimeString: '',
-            styles: {
-                fill: {
-                    background: 'lightgreen',
-                    position: 'relative',
-                    height: '5px',
-                    width: '0px'
-                }
+            fill: {
+                width: '0%'
+            },
+            thumb: {
+                left: '0%'
             }
         }
     },
@@ -50,8 +56,12 @@ export default {
             let min = Math.floor(this.currentTime/60);
             let sec = Math.floor(this.currentTime%60);
 
-            this.styles.fill.width = Math.floor((this.currentTime * 100) / this.duration)+ '%';
-            console.log(this.currentTime);
+            let position = this.currentTime * 100 / this.duration;
+        
+            if(position < 100) {
+                this.fill.width = position + '%';
+                this.thumb.left = (position - 2) + "%";
+            }
 
             if(sec >= 60)
                 sec = 0;
@@ -59,18 +69,53 @@ export default {
                 return `${min}:0${sec}`;
             else
                 return `${min}:${sec}`;
+        },
+        startScrub: function(event) {
+            console.log(event);
+        },
+        endScrub: function(event) {
+            console.log(event);
         }
     }
 }
 </script>
 
 <style>
-#length {
-    background: gray;
-    padding: 5px;
+.debug-box {
+    border: solid red 1px;
 }
 
-#thumb {
+.seekbar {
+    height: 20px;
+    width: 80%;
+    margin: auto;
+    position: relative;
+}
+
+.seekbar-fill {
+    height: 90%;
+    width: 0%;
+    background: lightgreen;
+    position: absolute;
+}
+
+.seekbar-length {
+    height: 90%;
+    width: 100%;
+    background: gray;
+}
+
+.seekbar-thumb {
+    height: 100%;
+    width: 5%;
+    border-radius: 50%;
+    bottom: 5%;
+    border: solid whitesmoke 1px;
+    position: absolute;
     background: green;
+}
+
+.seekbar-thumb:hover {
+    cursor: pointer;
 }
 </style>
